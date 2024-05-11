85f7a499a4f9f2a09e5f9ad76df0274fd76bb183
Стефан Христов <133797718+StefanHristov1997@users.noreply.github.com>
2024-05-11 16:44:02 +0300

    Start up

---
+++ Pathfinder/src/test/java/soft/uni/pathfinder/PathfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package soft.uni.pathfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathfinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
