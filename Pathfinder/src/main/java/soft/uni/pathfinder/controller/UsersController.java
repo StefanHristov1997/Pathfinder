45296cc6bcdc05029feb5639eafc8822367b3465
Стефан Христов <133797718+StefanHristov1997@users.noreply.github.com>
2024-05-13 16:37:03 +0300

    Connecting few controllers.

--- Pathfinder/src/main/java/soft/uni/pathfinder/controller/UsersController.java
+++ Pathfinder/src/main/java/soft/uni/pathfinder/controller/UsersController.java
@@ -3,13 +3,21 @@ package soft.uni.pathfinder.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/users")
-public class UserController {
+public class UsersController {
 
     @GetMapping("/login")
-    public String login() {
-        return "login";
+    public ModelAndView login() {
+        return new ModelAndView("login");
     }
+
+    @GetMapping("/register")
+    public ModelAndView register(){
+        return new ModelAndView("register");
+    }
+
+
 }
