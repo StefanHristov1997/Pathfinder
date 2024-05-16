b0893dbba38b56403b4da30bcff360e8aa4bfccb
Стефан Христов <133797718+StefanHristov1997@users.noreply.github.com>
2024-05-16 13:57:43 +0300

    Added user session.

--- Pathfinder/src/main/java/soft/uni/pathfinder/util/LoggedUser.java
+++ Pathfinder/src/main/java/soft/uni/pathfinder/util/LoggedUser.java
@@ -3,11 +3,13 @@ package soft.uni.pathfinder.util;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
 import soft.uni.pathfinder.model.entity.enums.UserRoleEnum;
 
 @Getter
 @Setter
 @Component
+@SessionScope
 public class LoggedUser {
 
     private String username;
