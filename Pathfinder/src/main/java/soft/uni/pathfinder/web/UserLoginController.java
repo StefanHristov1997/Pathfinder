07fe7074fa07a919f67a14c0ed0477b4e81253f9
Стефан Христов <133797718+StefanHristov1997@users.noreply.github.com>
2024-05-30 14:08:40 +0300

    Add login-error message.

--- Pathfinder/src/main/java/soft/uni/pathfinder/web/UserLoginController.java
+++ Pathfinder/src/main/java/soft/uni/pathfinder/web/UserLoginController.java
@@ -2,12 +2,10 @@ package soft.uni.pathfinder.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/users")
@@ -18,9 +16,10 @@ public class UserLoginController {
         return new ModelAndView("login");
     }
 
-    @PostMapping("/users/login-error")
+    @PostMapping("/login-error")
     public ModelAndView loginError(Model model) {
-        model.addAttribute("bad-credentials", true);
+        model.addAttribute("badCredentials", true);
+
         return new ModelAndView("login");
     }
 
