d6555607643b268e3764e9f67620cd6961f56ada
Стефан Христов <133797718+StefanHristov1997@users.noreply.github.com>
2024-05-30 13:57:44 +0300

    Add Post Method login-error

--- Pathfinder/src/main/java/soft/uni/pathfinder/web/UserLoginController.java
+++ Pathfinder/src/main/java/soft/uni/pathfinder/web/UserLoginController.java
@@ -1,8 +1,10 @@
 package soft.uni.pathfinder.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -16,13 +18,10 @@ public class UserLoginController {
         return new ModelAndView("login");
     }
 
-    @ExceptionHandler(LoginCredentialsException.class)
-    public ModelAndView handleLoginCredentialsError(LoginCredentialsException e,
-                                                    RedirectAttributes redirectAttributes) {
-
-        redirectAttributes.addFlashAttribute("badCredentials", true);
-        System.out.println(e.getMessage());
-        return new ModelAndView("redirect:login");
+    @PostMapping("/users/login-error")
+    public ModelAndView loginError(Model model) {
+        model.addAttribute("bad-credentials", true);
+        return new ModelAndView("login");
     }
 
 }
